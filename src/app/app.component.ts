a0d0d6f6ccce6a4348d806b5bb87709f3d70d6a6
Agilan Vageesan <agilanvageesan@gmail.com>
2021-05-11 00:39:04 +0530

    retro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'retro';
+export class AppComponent implements OnInit {
+  title = "retro";
+  currentText: string;
+  tasks: Array<string> = [];
+  ngOnInit() {
+    this.tasks.push("lala");
+    this.tasks.push("lets build a retro app");
+  }
+
+  AddRetroPoint() {
+    if (this.currentText.length > 1) {
+      this.tasks.push(this.currentText);
+      this.currentText = "";
+    }
+  }
+  DeleteTask(index){
+    this.tasks.splice(index,1)
+  }
+
 }
